6024edaf2fe3bcb4590b200914dccb7f214b33aa
supr3me <38699638+supr3meofficial@users.noreply.github.com>
2018-05-25 18:07:21 +0100

    test

--- cogs/owner.py
+++ cogs/owner.py
@@ -136,9 +136,7 @@ class OwnerCog:
 
 		for guild in self.bot.guilds:
 
-			embed.add_field(name=guild.name, value="Invites:", inline=False)
-			for invite in guild.invites():
-				embed.add_field(name='-',value=invite, inline=False)
+			embed.add_field(name=guild.name, value=guild.invites(), inline=False)
 
 		await ctx.send(embed=embed)
 
