7a9935cfc35a30b5cc96022d7d39f40f239f5026
supr3me <38699638+supr3meofficial@users.noreply.github.com>
2019-03-23 17:48:28 +0000

    Updated version

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ async def on_ready():
 		await bot.change_presence(status=discord.Status.online, activity=discord.Streaming(name="+help", url="http://twitch.tv/supr3meofficial"))
 		await asyncio.sleep(300)
 		print("\n[INFO] Changed bot's activity and status")
-		await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name="v2.0.3", type=3))
+		await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name="v2.1.0", type=3))
 		await asyncio.sleep(300)
 		print("\n[INFO] Changed bot's activity and status")
 		await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(name=connected_servers(), type=2))
@@ -219,4 +219,4 @@ async def on_ready():
 		await asyncio.sleep(300)
 		print("\n[INFO] Changed bot's activity and status")
 
-bot.run(os.environ['BOT_TOKEN'], bot=True, reconnect=True)
+bot.run('NDM3MzEwNTk2MTA3NDY4ODEw.De3vXg.h9CRL9epl_8kC2eLVVRCVD3RDIE', bot=True, reconnect=True)
